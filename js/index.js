8d944f6194bca13eedc9776d65019df44b3da819
champix56 <desorbaix@free.fr>
2022-05-17 10:31:23 +0200

    fonction de render DOM

--- js/index.js
+++ js/index.js
@@ -1,7 +1,37 @@
 /**
  * fonction d'init
  */
-function initJs(){
-    document.querySelector('footer').innerHTML='Orsys &copy; 2022';
+function initJs() {
+  document.querySelector("footer").innerHTML = "Orsys &copy; 2022";
 }
-document.addEventListener('DOMContentLoaded',initJs);
+document.addEventListener("DOMContentLoaded", initJs);
+var meme = new Meme();
+function renderSvg(nodeSvg, meme) {
+  nodeSvg.getAttributeNode("viewBox").value =
+    "0 0 " + meme.image.w + " " + meme.image.h;
+  /*image*/
+  var image = nodeSvg.querySelector("image");
+  image.getAttributeNodeNS("http://www.w3.org/1999/xlink", "href").value =
+    meme.image.url;
+  /*text*/
+  var text = nodeSvg.querySelector("text");
+  // recuperation d'un objet attribut
+  text.getAttributeNode("x").value = meme.dimension.x;
+  text.getAttributeNode("y").value = meme.dimension.y;
+  text.getAttributeNode("text-decoration").value = meme.underline
+    ? "uderline"
+    : "none";
+  // creation d'un attribut non existant
+  var a = document.createAttributeNS(
+    "http://www.w3.org/2000/svg",
+    "font-weight"
+  );
+  a.value = meme.fontWeight;
+  text.setAttributeNode(a);
+  //modif direct de la value d'un attribut existant
+  text.setAttribute("fill", meme.color);
+  text.setAttribute("font-style", meme.italic?'italic':'normal');
+  text.setAttribute("font-size", meme.fontSize);
+  text.innerHTML = meme.text;
+}
+renderSvg(document.querySelector("svg"), meme);
