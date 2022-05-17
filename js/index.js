93699557ff6e070cb5a1a64f8abfd9aba38a6592
champix56 <desorbaix@free.fr>
2022-05-17 12:44:19 +0200

    event du form

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 /**
  * fonction d'init
  */
+var meme;
+var nodeSvg;
 function initJs() {
   document.querySelector("footer").innerHTML = "Orsys &copy; 2022";
+  meme=new Meme(renderSvg);
+  nodeSvg=document.querySelector('svg');
+  addFormUpdateEvent(meme);
 }
 document.addEventListener("DOMContentLoaded", initJs);
-var meme = new Meme();
-function renderSvg(nodeSvg, meme) {
+
+function renderSvg(argsCaller, meme) {
+ var nodeSvg=argsCaller[0];
   nodeSvg.getAttributeNode("viewBox").value =
     "0 0 " + meme.image.w + " " + meme.image.h;
   /*image*/
@@ -34,4 +40,44 @@ function renderSvg(nodeSvg, meme) {
   text.setAttribute("font-size", meme.fontSize);
   text.innerHTML = meme.text;
 }
-renderSvg(document.querySelector("svg"), meme);
+
+function addFormUpdateEvent(meme){
+    var f=document.forms['meme-editor'];
+    f.addEventListener('submit',function(evt){
+        evt.preventDefault();
+        console.log('soumission du formulaire');
+        //enregistrement  REST
+    });
+    f['text'].addEventListener('change',function(evt){
+        meme.text=evt.target.value;
+        meme.render(nodeSvg);
+    });
+    f['color'].addEventListener('change',function(evt){
+        meme.color=evt.target.value;
+        meme.render(nodeSvg);
+    });
+    f['x'].addEventListener('change',function(evt){
+        meme.dimension.x=Number(evt.target.value);
+        meme.render(nodeSvg);
+    });
+    f['y'].addEventListener('change',function(evt){
+        meme.dimension.y=Number(evt.target.value);
+        meme.render(nodeSvg);
+    });
+    f['size'].addEventListener('change',function(evt){
+        meme.fontSize=Number(evt.target.value);
+        meme.render(nodeSvg);
+    });
+    f['weight'].addEventListener('change',function(evt){
+        meme.weight=evt.target.value;
+        meme.render(nodeSvg);
+    });
+    f['underline'].addEventListener('change',function(evt){
+        meme.underline=evt.target.checked;
+        meme.render(nodeSvg);
+    });
+    f['italic'].addEventListener('change',function(evt){
+        meme.italic=evt.target.checked;
+        meme.render(nodeSvg);
+    });
+}
