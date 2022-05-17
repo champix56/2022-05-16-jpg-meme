067b71f7ca7cfcc519c5a19f06dbf5030a6cf2b7
champix56 <desorbaix@free.fr>
2022-05-17 10:31:12 +0200

    metier meme

---
+++ js/meme/meme.js
@@ -0,0 +1,16 @@
+function Meme(renderCallback) {
+  this.text = "un text de meme";
+  this.dimension = { x: 0, y: 10 };
+  this.fontSize = 10;
+  this.fontWeight = "900";
+  this.underline = true;
+  this.italic = false;
+  this.color = "#ACACAC";
+  this.image = {
+    id: 0,
+    url: "/img/meme/futurama.webp",
+    w: 760,
+    h: 364,
+  };
+  this.render = renderCallback;
+}
