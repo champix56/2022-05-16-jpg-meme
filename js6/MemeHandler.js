c04192c1562129b555d7405d796a0838d900581d
champix56 <desorbaix@free.fr>
2022-05-18 17:10:27 +0200

    gestion timeout des promises

--- js6/MemeHandler.js
+++ js6/MemeHandler.js
@@ -1,5 +1,6 @@
 import {MemeArray, MemeImage} from './Meme6.js'
 import {ArrayImages} from './Image.js'
+
 class MemeGenerator{
     #memes;
     #images;
@@ -10,16 +11,27 @@ class MemeGenerator{
     loadInitial(){
         const prImages=this.#images.load();
         const prMemes=this.#memes.load();
-        Promise.all([prImages,prMemes])
+        const syncPromise=Promise.all([prImages,prMemes])
+        
+        const tmPromise=new Promise((resolve=(message)=>console.log(message))=>{
+            let wait = setTimeout(() => {
+                clearTimeout(wait);
+                resolve('Promise B win!');
+              }, 5)
+              return "error"
+        });
+        Promise.race([syncPromise,tmPromise])
         .then(arr_arr=>{
+            if(!Array.isArray(arr_arr))return;
             console.log(arr_arr);
-            arr_arr[1].map(memeUniqueJson=>{
+            arr_arr.map(memeUniqueJson=>{
                 const memeI=new MemeImage(JSON.stringify(memeUniqueJson));
-                memeI.image=arr_arr[0].find(img=>img.id===memeUniqueJson.imageId);
+                memeI.image=arr_arr.find(img=>img.id===memeUniqueJson.imageId);
                 this.#memes.push(memeI);
+                console.trace(this);
                 return {memes:this.#memes,images:this.#images}
             })
-        })
+        });
     }
 }
 export const memeGenerator=new MemeGenerator();
